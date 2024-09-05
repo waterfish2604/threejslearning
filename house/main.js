114aa663b6c1567034507670ffd5080bb6db218a
waterFish2604 <adityawankhede.and21@gmail.com>
2024-09-06 00:56:22 +0530

    more building

--- house/main.js
+++ house/main.js
@@ -1,9 +1,47 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 
 //create scene
 const scenary = new THREE.Scene();
 
 //ceating geometry and materail
+const wall = new THREE.BoxGeometry(1,1,1);
+const wallmat = new THREE.MeshBasicMaterial({
+    color: 'brown'
+})
 
+//creating mesh
+const build = new THREE.Mesh(wall, wallmat);
+
+scenary.add(build);
+
+//camera and renderer
+const aspectRatio = window.innerWidth/window.innerHeight;
+const cam = new THREE.PerspectiveCamera(65, aspectRatio, 0.1, 300);
+
+cam.position.z = 15;
+scenary.add(cam);
+
+const board = document.getElementsByClassName("canvas-to-draw");
+
+const render = new THREE.WebGLRenderer({
+    board
+})
+
+const control = new OrbitControls(cam, board);
+
+window.addEventListener('resize', ()=> {
+    render.setSize(window.innerWidth, window.innerHeight);
+    cam.aspect=window.innerWidth/window.innerHeight;
+    cam.updateProjectionMatrix();
+  })
+
+const renderloop = () => {
+    control.update();
+    render.render(scenary, cam);
+    window.requestAnimationFrame(renderloop);
+  }
+  
+  renderloop()
 
