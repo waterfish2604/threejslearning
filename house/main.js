7046069aa30248a85a9a00376d1949c8b4abd11b
waterFish2604 <adityawankhede.and21@gmail.com>
2024-09-06 19:41:38 +0530

    added object into build group and cementlayerX

--- house/main.js
+++ house/main.js
@@ -18,81 +18,98 @@ const cementM = new THREE.MeshBasicMaterial({
 
 //creating mesh
 const build0 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+build0.scale.set(2, 1.001, 1.001);
 
 const build1 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+build1.scale.set(2, 1.001, 1.001);
+build1.position.x= 2.1;
 
-const build3 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+const build2 = new THREE.Mesh(wall, wallmat);
+build2.scale.set(2, 1.001, 1.001);
+build2.position.x=4.2;
 
-const build4 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build4 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build5 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build5 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build6 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build6 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build7 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build7 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build8 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build8 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build9 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build9 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build10 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build10 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build11 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build11 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build12 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build12 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build13 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build13 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build14 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build14 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build15 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build15 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build16 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build16 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build17 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build17 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build18 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build18 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build19 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build19 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const build20 = new THREE.Mesh(wall, wallmat);
-build.scale.set(2, 1, 1.01);
+// const build20 = new THREE.Mesh(wall, wallmat);
+// build.scale.set(2, 1, 1.01);
 
-const cementSmallY = new THREE.Mesh(cementL, cementM);
-cementSmallY.position.x = 1.5- 0.5;
-cementSmallY.scale.set(0.25,1,1);
+const cementSmallY0 = new THREE.Mesh(cementL, cementM);
+cementSmallY0.position.x = 1;
+cementSmallY0.scale.set(0.25,1,1);
+
+const cementSmallY1 = new THREE.Mesh(cementL, cementM);
+cementSmallY1.position.x = 3.2;
+cementSmallY1.scale.set(0.25,1,1);
+
+const cementSmallY2 = new THREE.Mesh(cementL, cementM);
+cementSmallY2.position.x = 5.2;
+cementSmallY2.scale.set(0.25,1,1);
 
 const cementSmallX = new THREE.Mesh(cementL, cementM);
 cementSmallX.position.y=0.62;
 cementSmallX.position.x=4;
 cementSmallX.scale.set(10,0.25,1);
 
-//group og builds
+//group of builds
 const build = new THREE.Group();
 build.add(build0);
+build.add(build1);
+build.add(build2);
+
+const cementSmallYGroup = new THREE.Group();
+cementSmallYGroup.add(cementSmallY0);
+cementSmallYGroup.add(cementSmallY1);
+cementSmallYGroup.add(cementSmallY2);
 
 scenary.add(build);
-scenary.add(cementSmallY);
-scenary.add(cementSmallX)
+scenary.add(cementSmallYGroup);
+scenary.add(cementSmallX);
 
 //camera and renderer
 const aspectRatio = window.innerWidth/window.innerHeight;
@@ -100,6 +117,7 @@ const cam = new THREE.PerspectiveCamera(50, aspectRatio, 0.1, 300);
 
 cam.position.z = 15;
 cam.position.y = 5;
+cam.position.x = 5;
 scenary.add(cam);
 
 const board = document.querySelector(".canvas-to-draw")
