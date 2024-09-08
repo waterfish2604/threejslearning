4ad2aa201cb1f02b81cd0869ba6ebba7099d8f15
waterFish2604 <adityawankhede.and21@gmail.com>
2024-09-08 15:07:27 +0530

    on the way of completing wall1

--- house/main.js
+++ house/main.js
@@ -90,23 +90,99 @@ build13.scale.set(2, 1, 1.01);
 build13.position.y=2.5;
 build13.position.x=4.5;
 
-// const build15 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build14 = new THREE.Mesh(wall, wallmat);
+build14.scale.set(2, 1, 1.01);
+build14.position.y=2.5;
+build14.position.x=6.75;
 
-// const build16 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build15 = new THREE.Mesh(wall, wallmat);
+build15.scale.set(2, 1, 1.01);
+build15.position.y=2.5;
+build15.position.x=9;
 
-// const build17 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build16 = new THREE.Mesh(wall, wallmat);
+build16.scale.set(2, 1, 1.01);
+build16.position.y=3.75;
+build16.position.x=-1;
 
-// const build18 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build17 = new THREE.Mesh(wall, wallmat);
+build17.scale.set(2, 1, 1.01);
+build17.position.y=3.75;
+build17.position.x=1.25;
 
-// const build19 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build18 = new THREE.Mesh(wall, wallmat);
+build18.scale.set(2, 1, 1.01);
+build18.position.y=3.75;
+build18.position.x=3.5;
 
-// const build20 = new THREE.Mesh(wall, wallmat);
-// build.scale.set(2, 1, 1.01);
+const build19 = new THREE.Mesh(wall, wallmat);
+build19.scale.set(2, 1, 1.01);
+build19.position.y=3.75;
+build19.position.x=5.75;
+
+const build20 = new THREE.Mesh(wall, wallmat);
+build20.scale.set(2, 1, 1.01);
+build20.position.y=3.75;
+build20.position.x=8;
+
+const build21 = new THREE.Mesh(wall, wallmat);
+build21.scale.set(2, 1, 1.01);
+build21.position.y=3.75;
+build21.position.x=10.25;
+
+const build22 = new THREE.Mesh(wall, wallmat);
+build22.scale.set(2, 1, 1.01);
+build22.position.y=5;
+
+const build23 = new THREE.Mesh(wall, wallmat);
+build23.scale.set(2, 1, 1.01);
+build23.position.y=5;
+build23.position.x=2.25;
+
+const build24 = new THREE.Mesh(wall, wallmat);
+build24.scale.set(2, 1, 1.01);
+build24.position.y=5;
+build24.position.x=4.5;
+
+const build25 = new THREE.Mesh(wall, wallmat);
+build25.scale.set(2, 1, 1.01);
+build25.position.y=5;
+build25.position.x=6.75;
+
+const build26 = new THREE.Mesh(wall, wallmat);
+build26.scale.set(2, 1, 1.01);
+build26.position.y=5;
+build26.position.x=9;
+
+const build27 = new THREE.Mesh(wall, wallmat);
+build27.scale.set(2, 1, 1.01);
+build27.position.y=6.25;
+build27.position.x=-1;
+
+const build28 = new THREE.Mesh(wall, wallmat);
+build28.scale.set(2, 1, 1.01);
+build28.position.y=6.25;
+build28.position.x=1.25;
+
+const build29 = new THREE.Mesh(wall, wallmat);
+build29.scale.set(2, 1, 1.01);
+build29.position.y=6.25;
+build29.position.x=3.5;
+
+const build30 = new THREE.Mesh(wall, wallmat);
+build30.scale.set(2, 1, 1.01);
+build30.position.y=6.25;
+build30.position.x=5.75;
+
+const build31 = new THREE.Mesh(wall, wallmat);
+build31.scale.set(2, 1, 1.01);
+build31.position.y=6.25;
+build31.position.x=8;
+
+// const build32 = new THREE.Mesh(wall, wallmat);
+// build32.scale.set(2, 1, 1.01);
+// build32.position.y=6.25;
+// build32.position.x=-1;
 
 // pillars
 const pillar0 = new THREE.Mesh(pillarG, pillarM);
@@ -178,13 +254,36 @@ build.add(build10);
 build.add(build11);
 build.add(build12);
 build.add(build13);
-// build.add(build14);
-// build.add(build15);
-// build.add(build16);
-// build.add(build17);
-// build.add(build18);
-// build.add(build19);
+build.add(build14);
+build.add(build15);
+build.add(build16);
+build.add(build17);
+build.add(build18);
+build.add(build19);
+build.add(build20);
+build.add(build21);
+build.add(build22);
+build.add(build23);
+build.add(build24);
+build.add(build25);
+build.add(build26);
+build.add(build27);
+build.add(build28);
+build.add(build29);
+build.add(build30);
+build.add(build31);
 // build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+// build.add(build20);
+
 
 
 const cementSmallYGroup = new THREE.Group();
@@ -207,8 +306,8 @@ scenary.add(pillarGroup);
 const aspectRatio = window.innerWidth/window.innerHeight;
 const cam = new THREE.PerspectiveCamera(50, aspectRatio, 0.1, 300);
 
-cam.position.z = 15;
-cam.position.y = 10;
+cam.position.z = 17;
+cam.position.y = 15;
 cam.position.x = 15;
 scenary.add(cam);
 
